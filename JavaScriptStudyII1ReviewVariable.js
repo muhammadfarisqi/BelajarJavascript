72a399b2bf8971265e2077d9ee0e62441e102abd
muhammadfarisqi <muhammadfarisqiya@gmail.com>
2021-04-05 19:50:02 +0700

    Create JavaScriptStudyII1ReviewVariable.js

---
+++ JavaScriptStudyII1ReviewVariable.js
@@ -0,0 +1,11 @@
+// Deklarasikan variable number
+let number=1 
+
+// Print nilai dari variable number, dan tambahkan dengan 1
+console.log(number);
+
+// Salin kedua baris diatas dan tempelkan dibaris bawah sebanyak 4 kali
+console.log(number+=1);
+console.log(number+=1);
+console.log(number+=1);
+console.log(number+=1);
