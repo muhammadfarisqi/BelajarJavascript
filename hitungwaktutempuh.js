0e459d5cc550c0c06ed05a1515a6b91fc1c0283d
muhammadfarisqi <muhammadfarisqiya@gmail.com>
2021-04-19 08:41:07 +0700

    Kode jadi

---
+++ hitungwaktutempuh.js
@@ -0,0 +1,8 @@
+function main() {
+    var distance = parseInt(readLine(), 10);
+    //your code goes here
+    var speed = 40
+    var time = 60
+    var minutes = distance/speed
+console.log(minutes*time) 
+}
